54cf9d3adfaafa81ffb6f77438e4cbf884a43a9b
Felix Hammer <fhammer@uni-osnabrueck.de>
2024-05-09 12:37:43 +0100

    better parse yaml

--- parse_yaml.py
+++ parse_yaml.py
@@ -3,14 +3,17 @@ import os
 import argparse
 import subprocess
 from datetime import datetime
+from src.train_args import BasicArgs
 
-# Setting up the argument parser
+types =  {a:str(b.annotation) for a,b in BasicArgs.model_fields.items()}
+
 parser = argparse.ArgumentParser()
 parser.add_argument("path", type=str, help="Path to the YAML configuration file.")
 parser.add_argument("--job", type=int, default=None, help="Specify a job index to run a single job.")
 parser.add_argument("--p3", action="store_true")
+parser.add_argument("--o", type=str, default="runs.sh")
 
-print("Hey (:")
+
 
 args = parser.parse_args()
 
@@ -22,16 +25,31 @@ with open(args.path, "r") as f:
 common_settings = config["common"]
 runs = config["runs"] if args.job is None else [config["runs"][args.job]]
 
+
+commands = []
+
 for index, run in enumerate(runs):
     command = [python_path, "src/script.py"]
     for key, value in common_settings.items():
-        command.extend([f"--{key}", str(value)])
+        
+        s_val = str(value)
+        if types[key] in [str(type("a")), 'typing.Optional[str]']:
+            s_val = '"' + s_val + '"'
+        
+        command.extend([f"--{key}", str(s_val)])
+    
     for key, value in run.items():
-        command.extend([f"--{key}", str(value)])
+        s_val = str(value)
+        if types[key] in [str(type("a")), 'typing.Optional[str]']:
+            s_val = '"' + s_val + '"'
+        command.extend([f"--{key}", str(s_val)])
     
     log_file = f"{run['run_name']}_{datetime.now().strftime('%d-%H:%M')}.log".replace(" ","_")
-    with open(log_file, "w") as log:
-        process = subprocess.Popen(command, stdout=log, stderr=subprocess.STDOUT)
-        process.wait()
-        print(f"Run {index+1} logged to {log_file}")
+    command.extend([">", log_file, "2>&1"])
+
+    commands.append(" ".join(command))
+
+
+with open(args.o , "w") as f:
+    f.write("\n".join(commands))
 
